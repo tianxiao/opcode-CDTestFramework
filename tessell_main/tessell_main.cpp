b98d27eb9f74855c8a7836e83466e3b111290b31
tianxiao <tianxiao987@gmail.com>
2013-04-16 15:21:11 +0800

    Add the initial configure file m1dump.txt is the tetra topology info m28dump.txt is the oct topology info

--- tessell_main/tessell_main.cpp
+++ tessell_main/tessell_main.cpp
@@ -10,22 +10,36 @@ MeshData meshdata;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	MeshDataConfigTetra(&meshdata);
-	MeshDataOuputOFF(&meshdata,"m1.off");
+	//MeshDataConfigTetra(&meshdata);
+	//MeshDataOuputOFF(&meshdata,"m1.off");
+	//MeshDataDump(&meshdata,"m1dump.txt");
 
-	txBasicMesh bm(&meshdata);
-	bm.SubdivsionMesh();
-	MeshData *m2 = bm.GetSubdivsionMeshData();
-	MeshDataOuputOFF(m2,"m2.off");
 
-	txBasicMesh bm2(m2);
-	bm2.SubdivsionMesh();
-	MeshData *m3 = bm2.GetSubdivsionMeshData();
-	MeshDataOuputOFF(m3, "m3.off");
+	//txBasicMesh bm(&meshdata);
+	//bm.SubdivsionMesh();
+	//MeshData *m2 = bm.GetSubdivsionMeshData();
+	//MeshDataOuputOFF(m2,"m2.off");
+	//MeshDataDump(m2,"m2dump.txt");
 
-	txBasicMesh bm3(m3);
-	bm3.SubdivsionMesh();
-	MeshData *m4 = bm3.GetSubdivsionMeshData();
+	//txBasicMesh bm2(m2);
+	//bm2.SubdivsionMesh();
+	//MeshData *m3 = bm2.GetSubdivsionMeshData();
+	//MeshDataOuputOFF(m3, "m3.off");
+
+	//txBasicMesh bm3(m3);
+	//bm3.SubdivsionMesh();
+	//MeshData *m4 = bm3.GetSubdivsionMeshData();
+
+	MeshDataConfigOct(&meshdata);
+	MeshDataDump(&meshdata,"m28dump.txt");
+	
+	txBasicMesh om0(&meshdata);
+	om0.SubdivsionMesh();
+	MeshDataOuputOFF(om0.GetSubdivsionMeshData(),"mOct0.off");
+
+	//txBasicMesh om1(om0.GetSubdivsionMeshData());
+	//om1.SubdivsionMesh();
+	//MeshDataOuputOFF(om1.GetSubdivsionMeshData(),"mOct1.off");
 
 	return 0;
 }
