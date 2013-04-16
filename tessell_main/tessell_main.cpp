ab7595a61362149e1bab527c900131023c7e775d
tianxiao <tianxiao987@gmail.com>
2013-04-16 20:08:40 +0800

    Finished the code

--- tessell_main/tessell_main.cpp
+++ tessell_main/tessell_main.cpp
@@ -10,36 +10,65 @@ MeshData meshdata;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	MeshDataConfigTetra(&meshdata);
-	MeshDataOuputOFF(&meshdata,"m1.off");
-	MeshDataDump(&meshdata,"m1dump.txt");
+	//MeshDataConfigTetra(&meshdata);
+	//MeshDataOuputOFF(&meshdata,"m1.off");
+	//MeshDataDump(&meshdata,"m1dump.txt");
 
 
-	txBasicMesh bm(&meshdata);
-	bm.SubdivsionMesh();
-	MeshData *m2 = bm.GetSubdivsionMeshData();
-	MeshDataOuputOFF(m2,"m2.off");
-	MeshDataDump(m2,"m2dump.txt");
+	//txBasicMesh bm(&meshdata);
+	//bm.SubdivsionMesh();
+	//MeshData *m2 = bm.GetSubdivsionMeshData();
+	//MeshDataOuputOFF(m2,"m2.off");
+	//MeshDataDump(m2,"m2dump.txt");
 
-	txBasicMesh bm2(m2);
-	bm2.SubdivsionMesh();
-	MeshData *m3 = bm2.GetSubdivsionMeshData();
-	MeshDataOuputOFF(m3, "m3.off");
+	//txBasicMesh bm2(m2);
+	//bm2.SubdivsionMesh();
+	//MeshData *m3 = bm2.GetSubdivsionMeshData();
+	//MeshDataOuputOFF(m3, "m3.off");
 
-	txBasicMesh bm3(m3);
-	bm3.SubdivsionMesh();
-	MeshData *m4 = bm3.GetSubdivsionMeshData();
+	//txBasicMesh bm3(m3);
+	//bm3.SubdivsionMesh();
+	//MeshData *m4 = bm3.GetSubdivsionMeshData();
 
-	//MeshDataConfigOct(&meshdata);
-	//MeshDataDump(&meshdata,"m28dump.txt");
-	//
-	//txBasicMesh om0(&meshdata);
-	//om0.SubdivsionMesh();
-	//MeshDataOuputOFF(om0.GetSubdivsionMeshData(),"mOct0.off");
+	//-------------------OCT
 
-	//txBasicMesh om1(om0.GetSubdivsionMeshData());
-	//om1.SubdivsionMesh();
-	//MeshDataOuputOFF(om1.GetSubdivsionMeshData(),"mOct1.off");
+	MeshDataConfigOct(&meshdata);
+	MeshDataDump(&meshdata,"m28dump.txt");
+	
+	txBasicMesh om0(&meshdata);
+	om0.SubdivsionMesh();
+	MeshDataOuputOFF(om0.GetSubdivsionMeshData(),"mOct0.off");
+
+	txBasicMesh om1(om0.GetSubdivsionMeshData());
+	om1.SubdivsionMesh();
+	MeshDataOuputOFF(om1.GetSubdivsionMeshData(),"mOct1.off");
+
+	txBasicMesh om2(om1.GetSubdivsionMeshData());
+	om2.SubdivsionMesh();
+	MeshDataOuputOFF(om2.GetSubdivsionMeshData(),"mOct2.off");
+
+	txBasicMesh om3(om2.GetSubdivsionMeshData());
+	om3.SubdivsionMesh();
+	MeshDataOuputOFF(om3.GetSubdivsionMeshData(),"mOct3.off");
+
+	txBasicMesh om4(om3.GetSubdivsionMeshData());
+	om4.SubdivsionMesh();
+	MeshDataOuputOFF(om4.GetSubdivsionMeshData(),"mOct4.off");
+
+	txBasicMesh om5(om4.GetSubdivsionMeshData());
+	om5.SubdivsionMesh();
+	MeshDataOuputOFF(om5.GetSubdivsionMeshData(),"mOct5.off");
+
+	txBasicMesh om6(om5.GetSubdivsionMeshData());
+	om6.SubdivsionMesh();
+	MeshDataOuputOFF(om6.GetSubdivsionMeshData(),"mOct6.off");
+
+	//txBasicMesh recur(&meshdata);
+	//for (size_t i=0; i<3; i++) {
+	//	recur.SubdivsionMesh();
+	//	txBasicMesh inter(recur.GetSubdivsionMeshData());
+	//	inter.SubdivsionMesh();
+	//}
 
 	return 0;
 }
